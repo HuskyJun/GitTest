35652e60ada3e69e06ad566351cc684bbcd99b8f
Euijun Han <hanuijun59@gmail.com>
2016-08-26 14:00:57 -0400

    Added - Second line

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -11,6 +11,7 @@ namespace GitTest
         static void Main(string[] args)
         {
             Console.WriteLine("First line added");
+            Console.WriteLine("Second line added");
         }
     }
 }
